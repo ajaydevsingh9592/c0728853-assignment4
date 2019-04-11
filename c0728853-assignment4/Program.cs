d076115549d59cf20284dc14d7ac138858615e4d
ajaydevsingh9592 <46795478+ajaydevsingh9592@users.noreply.github.com>
2019-04-11 14:40:53 -0400

    2:40 commit

--- c0728853-assignment4/Program.cs
+++ c0728853-assignment4/Program.cs
@@ -1,16 +1,62 @@
-﻿using System;
+using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace c0728853_assignment4
+namespace C0725450_Assignment4
 {
     class Program
     {
+        ArrayList Beowulf;
         static void Main(string[] args)
         {
-            ArrayList Beowulf;
+            Program p = new Program();
+            p.Beowulf = new ArrayList();
+        }
+
+        public void Run()
+        {
+            this.ReadTextFiles();
+        }
+
+        public void ReadTextFiles()
+        {
+            //Read file using StreamReader. Reads file line by line
+            using (StreamReader file = new StreamReader("Desktop/beowulf.txt"))
+            {
+                int counter = 0;
+                string ln;
+
+                while ((ln = file.ReadLine()) != null)
+                {
+                    Console.WriteLine(ln);
+                    Beowulf.Add(ln);
+                }
+                file.Close();
+                Console.WriteLine($"File has {counter} lines.");
+            }
+        }
+
+        public int FindNumberOfBlankSpaces(string line)
+        {
+            int countletters = 0;
+            int countSpaces = 0;
+
+            foreach (char c in line)
+            {
+                if (char.IsLetter(c))
+                {
+                    countletters++;
+                }
+                if (char.IsWhiteSpace(c))
+                {
+                    countSpaces++;
+                }
+                return countSpaces;
+            }
         }
     }
 }
